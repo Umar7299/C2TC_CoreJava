6d9fe34a8bac0765469a18b1c54916b5acd7b127
UMAR ALI <UMAR ALI@DESKTOP-O9O7QNL>
2023-07-10 10:04:09 +0530

    first commit

--- C13/src/com/cg/dayone/Emojis.java
+++ C13/src/com/cg/dayone/Emojis.java
@@ -5,6 +5,16 @@ public class Emojis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("hai");
+	System.out.println("\uD83D\uDE00"); // Grinning face
+    System.out.println("\uD83D\uDE1C"); // Cool face
+    System.out.println("\uD83D\uDE04"); // Smiling face with open mouth and smiling eyes
+    System.out.println("\uD83D\uDE2D"); // Loudly crying face
+    System.out.println("\uD83D\uDC68\u200D\uD83D\uDCBB"); // Man and woman holding hands
+    System.out.println("\uD83C\uDFB8"); // Musical notes
+    System.out.println("\uD83C\uDFC8"); // Trophy
+    System.out.println("\uD83C\uDF89"); // Party popper
+    System.out.println("\uD83D\uDCBB"); // Personal computer
+    System.out.println("\uD83C\uDFE0"); // House with garden
 
 	}
 
