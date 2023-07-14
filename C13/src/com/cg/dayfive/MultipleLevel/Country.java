988b89d6da59ee989b74922b6ec21ff50dce26f2
UMAR ALI <UMAR ALI@DESKTOP-O9O7QNL>
2023-07-14 20:28:54 +0530

    commit

---
+++ C13/src/com/cg/dayfive/MultipleLevel/Country.java
@@ -0,0 +1,26 @@
+package com.cg.dayfive.MultipleLevel;
+
+public class Country {
+
+	private String countryName;
+	private long countryPopulation;
+	
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public long getCountryPopulation() {
+		return countryPopulation;
+	}
+	public void setCountryPopulation(long countryPopulation) {
+		this.countryPopulation = countryPopulation;
+	}
+	
+	@Override
+	public String toString() {
+		return "Country Details [countryName = " + countryName + ", countryPopulation = " + countryPopulation + "]";
+	}
+
+}
---
+++ C13/src/com/cg/dayfive/SingleLevel/SingleDemo.java
@@ -0,0 +1,20 @@
+package com.cg.dayfive.SingleLevel;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student();
+		
+		s.setName("UMAR ALI");
+		s.setCollege("MSAJCE");
+		s.setAadhar("678045634510");
+		s.setArea("COIMBATORE");
+		s.setPhNo(72345678);
+		s.setRollNo(4018);
+		
+		System.out.println(s);
+
+	}
+
+}
