c771183456900c1e7521355bfc7d9949c9e9a7b4
UMAR ALI <UMAR ALI@DESKTOP-O9O7QNL>
2023-07-14 20:05:10 +0530

    commit

---
+++ C13/src/com/cg/dayfour/ConstructorDemo.java
@@ -0,0 +1,15 @@
+package com.cg.dayfour;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		
+		Constructor cons = new Constructor();
+		
+		System.out.println("Main Print Statement");
+		
+		Constructor cons1 = new Constructor(6);
+		
+	}
+
+}
